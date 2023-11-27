29d0188f6339b0faa2ece31c894787f1a598c394
CraciDenisStefan <crdenis13@gmail.com>
2023-11-27 14:42:20 +0100

    calculator ok

--- src/main/java/org/java/Calculator.java
+++ src/main/java/org/java/Calculator.java
@@ -1,5 +1,24 @@
 package org.java;
 
 public class Calculator {
+	
+	 public float somma(float num1, float num2) {
+	        return num1 + num2;
+	    }
+
+	 public float sottrai(float num1, float num2) {
+	        return num1 - num2;
+	    }
+
+	 public float dividi(float num1, float num2) {
+	        if (num2 == 0) {
+	            throw new IllegalArgumentException("non puoi dividere per 0");
+	        }
+	        return num1 / num2;
+	    }
+
+	 public float moltiplica(float num1, float num2) {
+	        return num1 * num2;
+	    }
 
 }
--- src/test/java/org/java/CalculatorTest.java
+++ src/test/java/org/java/CalculatorTest.java
@@ -1,5 +1,36 @@
 package org.java;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+	
+	@Test
+    public void testSomma() {
+        Calculator calculator = new Calculator();
+        float risultato = calculator.somma(5, 7);
+        Assertions.assertEquals(12, risultato, "errore nel metodo somma");
+    }
+
+    @Test
+    public void testSottrai() {
+        Calculator calculator = new Calculator();
+        float risultato = calculator.sottrai(10, 4);
+        Assertions.assertEquals(6, risultato, "errore nel metodo sottrai");
+    }
+
+    @Test
+    public void testDividi() {
+        Calculator calculator = new Calculator();
+        float risultato = calculator.dividi(8, 2);
+        Assertions.assertEquals(4, risultato, "errore nel metodo dividi");
+    }
+
+    @Test
+    public void testMoltiplica() {
+        Calculator calculator = new Calculator();
+        float risultato = calculator.moltiplica(3, 6);
+        Assertions.assertEquals(18, risultato, "errore nel metodo moltiplica");
+    }
 
 }
